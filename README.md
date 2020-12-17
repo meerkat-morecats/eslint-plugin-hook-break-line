df80a7cdf8fdcb659f0991095d867f748ddeb559
kangkai04 <kangkai04@baidu.com>
2020-12-17 21:05:30 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# eslint-plugin-hook-break-line
+
+react hook arguments  break line
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-hook-break-line`:
+
+```
+$ npm install eslint-plugin-hook-break-line --save-dev
+```
---
+++ tests/lib/rules/eslint-plugin-hook-break-line.js
@@ -0,0 +1,39 @@
+/**
+ * @fileoverview react hook arguments  break line
+ * @author meerkat-morecat
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/eslint-plugin-hook-break-line"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("eslint-plugin-hook-break-line", rule, {
+
+    valid: [
+        "useDemo(\nargument1,\n[argument2]\n)",
+        "f(1,2)",
+        "f(1,[2])",
+        "useDemo(argument1,argument2)",
+        // give me some code that won't trigger a warning
+    ],
+
+    invalid: [
+        {
+            code:  "useDemo(argument1,[argument2])",
+            errors: [{
+                messageId: 'hookArgumentsBreakLine',
+            }]
+        }
+    ]
+});
